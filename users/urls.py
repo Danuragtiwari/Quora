26073d9256f38c5de75f967c15494aafc0fee30a
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-08-12 16:08:54 +0530

    users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
  path('login/', LoginView.as_view(template_name='app/login.html'), name="login"),
 
  # Logout Page
- path('logout/', views.logout_view, name='logout' ),
+ path('logout', views.logout_view, name='logout' ),
 
  # Registration Page
- path('register/', views.register, name='register'),
+ path('register', views.register, name='register'),
 
 ]
